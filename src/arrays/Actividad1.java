7fb68a7b9958302bcbbce23ffbf9748a72aa2a07
Mery <mchozamerino@gmail.com>
2023-01-11 18:05:19 +0100

    nuevos ejercicios

---
+++ src/arrays/Actividad1.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class Actividad1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		/*
+		 * 1- Crea una clase ArrayCuenta que defina un array de 10 números enteros
+		 * inicializados a 0 y luego en un bucle solicite al usuario que introduzca cada
+		 * elemento. Luego crea otro bucle que contabilice el total de números
+		 * positivos, negativos y 0.
+		 */
+		
+		int[10] array1;
+		
+		System.out.println(Arrays.toString(array1));
+		
+		for (int i=0; i<array1.length;i++) {
+			
+		}
+
+	}
+
+}
