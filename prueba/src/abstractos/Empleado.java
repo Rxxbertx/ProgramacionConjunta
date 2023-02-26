444555a478ca10e412d381e70835853e11341722
Mery <mchozamerino@gmail.com>
2023-02-26 19:14:58 +0100

    abstracts

--- prueba/src/abstractos/Empleado.java
+++ prueba/src/abstractos/Empleado.java
@@ -1,5 +1,61 @@
 package abstractos;
 
-public class Empleado {
+public class Empleado extends Persona{
 
+	//Atributos
+	public int id;
+	public String descripcion;
+	
+	//Constructor por defecto 
+	
+	public Empleado() {
+		super();
+	}
+	
+	//Constructor con parámetros
+	public Empleado(String nombre, int id) {
+		super(nombre);
+		this.id=id;
+	}
+	
+	//Getters y setters
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	
+	//Método abstracto
+	
+	public String dameDescripcion() {
+		return descripcion;
+	}
+	
+	//Sobreescribo el método dameNombre para que me de también el id
+	
+	@Override
+	public static String dameNombre() {
+		return super.dameNombre();
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
